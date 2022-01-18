f26dbb6e44bea4bdafc54e0cc7f6c0bd4d2c464e
yoonsun jang <bakhagirl@naver.com>
2022-01-18 18:39:30 +0900

    윤선

---
+++ src/main/java/service/RootService.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.util.List;
+
+import criteria.SearchCriteria;
+import vo.RootVO;
+import vo.PageVO;
+
+public interface RootService {
+	
+	// ** Board Check List
+	List<RootVO> checkList(RootVO vo);
+	
+	// ** Ajax id_BoardList
+	List<RootVO> aidBList(RootVO vo);
+	
+	// ** PageList 2. SearchCriteria PageList
+	int searchRowsCount(SearchCriteria cri) ;
+	List<RootVO> searchList(SearchCriteria cri) ;
+	// ** PageList1.
+	PageVO<RootVO> pageList(PageVO<RootVO> pvo); // pageList 1 
+	// **  CRUD
+	List<RootVO> selectList(); //selectList
+	RootVO selectOne(RootVO vo); //selectOne
+	int insert(RootVO vo); //insert
+	int update(RootVO vo); //update
+	int delete(RootVO vo); //delete
+	// ** 조회수증가
+	int countUp(RootVO vo); //countUp
+	// ** 답글입력
+	//int rinsert(RootVO vo);
+
+}
---
+++ src/main/java/service/RootServiceImpl.java
@@ -0,0 +1,86 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import criteria.SearchCriteria;
+import mapperInterface.RootMapper;
+//import util.RootDAO;
+import vo.RootVO;
+import vo.PageVO;
+
+//** interface 자동완성 
+//=> Alt + Shift + T  
+//=> 또는 마우스우클릭 PopUp Menu 의  Refactor - Extract Interface...
+
+@Service
+public class RootServiceImpl implements RootService {
+	
+	@Autowired
+	RootMapper dao;
+	
+	// ** Board Check List
+	@Override
+	public List<RootVO> checkList(RootVO vo) {
+		return dao.checkList(vo);
+	}
+	
+	//** Ajax id_BoardList
+	@Override
+	public List<RootVO> aidBList(RootVO vo) {
+		return dao.aidBList(vo);
+	}
+	
+	// ** PageList 2. SearchCriteria PageList
+	@Override
+	public int searchRowsCount(SearchCriteria cri) {
+		return dao.searchRowsCount(cri);
+	} 
+	@Override
+	public List<RootVO> searchList(SearchCriteria cri) {
+		return dao.searchList(cri) ;
+	}
+	// ** PageList1.
+	@Override
+	public PageVO<RootVO> pageList(PageVO<RootVO> pvo) {
+		pvo.setTotalRowCount(dao.totalRowCount());
+		pvo.setList(dao.pageList(pvo));
+		return pvo;
+	} //pageList
+	
+	@Override
+	public List<RootVO> selectList() {
+		return dao.selectList();
+	} //selectList
+	@Override
+	public RootVO selectOne(RootVO vo) {
+		return dao.selectOne(vo);
+	} //selectOne
+
+	@Override
+	public int insert(RootVO vo) {
+		return dao.insert(vo) ;
+	} //insert
+	@Override
+	public int update(RootVO vo) {
+		return dao.update(vo) ;
+	} //update
+	@Override
+	public int delete(RootVO vo) {
+		return dao.delete(vo) ;
+	} //delete
+	
+	// ** 조회수증가
+	@Override
+	public int countUp(RootVO vo) {
+		return dao.countUp(vo) ;
+	} //countUp
+	
+	// ** 답글입력
+	/*
+	 * @Override public int rinsert(RootVO vo) { return dao.rinsert(vo); }
+	 */
+} //class
+
