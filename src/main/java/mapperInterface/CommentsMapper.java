55f58ad45927bdc9057e5a4f82f2d72ddfe9737f
Ji Hyun Jung <noeverjjh@gmail.com>
2022-01-18 16:48:00 +0900

    comments

--- src/main/java/mapperInterface/CommentsMapper.java
+++ src/main/java/mapperInterface/CommentsMapper.java
@@ -8,13 +8,6 @@ import vo.PageVO;
 
 public interface CommentsMapper {
 	
-	// ** Comments Check List
-	List<CommentsVO> checkList(CommentsVO vo);
-	
-	// ** Ajax id_CommentsList
-	List<CommentsVO> aidBList(CommentsVO vo) ;
-
-	// ** PageList 2. SearchCriteria PageList
 	int searchRowsCount(SearchCriteria cri) ;
 	List<CommentsVO> searchList(SearchCriteria cri) ;
 	// ** PageList1.
@@ -26,9 +19,5 @@ public interface CommentsMapper {
 	int insert(CommentsVO vo); //insert
 	int update(CommentsVO vo); //update
 	int delete(CommentsVO vo); //delete
-	// ** 조회수증가
-	int countUp(CommentsVO vo); //countUp
-	// ** 답글입력
-	int rinsert(CommentsVO vo);
 	
 } //interface
---
+++ src/main/java/service/CommentsServiceimpl.java
@@ -0,0 +1,61 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import critetia.SearchCriteria;
+import mapperInterface.CommentsMapper;
+import vo.CommentsVO;
+import vo.PageVO;
+
+@Service
+public class CommentsServiceimpl implements CommentsService{
+	
+	@Autowired
+	CommentsMapper dao;
+	
+	@Override
+	public int searchRowsCount(SearchCriteria cri) {
+		return dao.searchRowsCount(cri);
+	}
+
+	@Override
+	public List<CommentsVO> searchList(SearchCriteria cri) {
+		return dao.searchList(cri) ;
+	}
+
+	@Override
+	public PageVO<CommentsVO> pageList(PageVO<CommentsVO> pvo) {
+		pvo.setTotalRowCount(dao.totalRowCount());
+		pvo.setList(dao.pageList(pvo));
+		return pvo;
+	}
+
+	@Override
+	public List<CommentsVO> selectList() {
+		return dao.selectList();
+	}
+
+	@Override
+	public CommentsVO selectOne(CommentsVO vo) {
+		return dao.selectOne(vo);
+	}
+
+	@Override
+	public int insert(CommentsVO vo) {
+		return dao.insert(vo) ;
+	}
+
+	@Override
+	public int update(CommentsVO vo) {
+		return dao.update(vo) ;
+	}
+
+	@Override
+	public int delete(CommentsVO vo) {
+		return dao.delete(vo) ;
+	}
+
+}
